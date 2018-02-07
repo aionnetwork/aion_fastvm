97d0b1ae07224f858e5ed572a789fb9f68d8b67b
Yulong Wu <yulong@nuco.io>
2018-02-07 17:12:27 -0500

    Add license header

--- java/src/org/aion/fastvm/Callback.java
+++ java/src/org/aion/fastvm/Callback.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * Copyright (c) 2017 Aion foundation.
+ * Copyright (c) 2017-2018 Aion foundation.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
