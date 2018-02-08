18af3cdda6c793d791c82da1cca0dbffe00e26e5
Yulong Wu <yulong@nuco.io>
2018-02-08 16:57:11 -0500

    Move release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# build fastvm and java bindings
-cd .. && make && cd dist
-cd .. && make jni && cd dist
+cd "$(dirname "$0")/.."
+
+# build fastvm and solidity compiler
+make
+make jni
+cd solidity && make && cd ../dist
 
 # copy LLVM library
 cp /usr/lib/x86_64-linux-gnu/libLLVM-4.0.so.1 .
@@ -18,17 +21,12 @@ END
 tar -czf fastvm_v0.0.0.tar.gz file.list libLLVM-4.0.so.1 libevmjit.so libfastvm.so
 
 
-
-# build compiler
-cd ../solidity && make && cd ../dist
-
 # copy libraries
 cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.58.0 .
 cp /usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.58.0 .
 cp /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.58.0 .
 cp /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 .
 cp /usr/lib/x86_64-linux-gnu/libjsoncpp.so.1 .
-
 
 # construct the file list
 tee file.list << END
@@ -40,4 +38,4 @@ libjsoncpp.so.1
 END
 
 # make archieve
-tar -czf compiler_v0.0.0.tar.gz file.list libboost* libjsoncpp.so.1 solc
+tar -czf solidity_v0.0.0.tar.gz file.list libboost* libjsoncpp.so.1 solc
