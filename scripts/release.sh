ff1b0c53bde10fe918f263042bff650803b2e64a
Yulong Wu <yulong@nuco.io>
2018-09-06 14:22:59 -0400

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 
 if [ $# -eq 2 ]; then
-    cd aion_fastvm 
-else
+    cd aion_fastvm
+elif [ $# -eq 1 ]; then
     cd "$(dirname "$0")/.."
+else
+    echo "Usage: ./release.sh [VERSION]"
+    exit 1
 fi
 
 fastvm_dir="fastvm_v$1"
 solidity_dir="solidity_v$1"
 
-# build fastvm and solidity compiler
-make
-make jni
-cd solidity && make  && cd ../dist
+# build fastvm
+make evmjit jni
 
 # copy libraries
-rm -r ${fastvm_dir}
-mkdir ${fastvm_dir} && cd ${fastvm_dir}
-cp /usr/lib/x86_64-linux-gnu/libLLVM-4.0.so.1 .
-cp /usr/lib/x86_64-linux-gnu/libffi.so.6 .
-cp /usr/lib/x86_64-linux-gnu/libedit.so.2 .
-cd ..
-cp libevmjit.so ${fastvm_dir}
-cp libfastvm.so ${fastvm_dir}
-cd ${fastvm_dir}
+cd dist
+mkdir -p ${fastvm_dir} && cd ${fastvm_dir}
+cp ../libfastvm.so . || exit 2
+cp ../libevmjit.so . || exit 2
+cp /usr/lib/x86_64-linux-gnu/libLLVM-4.0.so.1 . || exit 2
+# included in the `common` library
+# cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 . || exit 2
+# cp /lib/x86_64-linux-gnu/libgcc_s.so.1 . || exit 2
+cp /usr/lib/x86_64-linux-gnu/libffi.so.6 . || exit 2
+cp /usr/lib/x86_64-linux-gnu/libedit.so.2 . || exit 2
+cp /lib/x86_64-linux-gnu/libtinfo.so.5 . || exit 2
+cp /lib/x86_64-linux-gnu/libz.so.1 . || exit 2
+cp /lib/x86_64-linux-gnu/libbsd.so.0 . || exit 2
 
 # construct the file list
 tee file.list << END
-libffi.so.6
+libbsd.so.0
+libz.so.1
+libtinfo.so.5
 libedit.so.2
+libffi.so.6
 libLLVM-4.0.so.1
 libevmjit.so
 libfastvm.so
@@ -38,26 +46,34 @@ END
 cd ..
 tar -czf ${fastvm_dir}.tar.gz ${fastvm_dir}
 
+# compile solidity
+cd ../solidity && make && cd ../dist
 
 # copy libraries
-rm -r ${solidity_dir} 
-mkdir ${solidity_dir} && cd ${solidity_dir}
-cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.58.0 .
-cp /usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.58.0 .
-cp /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.58.0 .
-cp /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 .
-cp /usr/lib/x86_64-linux-gnu/libjsoncpp.so.1 .
-cd ..
-cp solc ${fastvm_dir}
-cd  ${fastvm_dir}
+mkdir -p ${solidity_dir} && cd ${solidity_dir}
+cp ../solc . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libboost_filesystem.so.1.58.0 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libboost_program_options.so.1.58.0 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libboost_regex.so.1.58.0 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libboost_system.so.1.58.0 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libjsoncpp.so.1 . || exit 3
+# included in the `common` library
+# cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 . || exit 3
+# cp /lib/x86_64-linux-gnu/libgcc_s.so.1 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libicui18n.so.55 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libicuuc.so.55 . || exit 3
+cp /usr/lib/x86_64-linux-gnu/libicudata.so.55 . || exit 3
 
 # construct the file list
 tee file.list << END
-libboost_system.so.1.58.0
-libboost_filesystem.so.1.58.0
-libboost_program_options.so.1.58.0
-libboost_regex.so.1.58.0
+libicudata.so.55
+libicuuc.so.55
+libicui18n.so.55
 libjsoncpp.so.1
+libboost_system.so.1.58.0
+libboost_regex.so.1.58.0
+libboost_program_options.so.1.58.0
+libboost_filesystem.so.1.58.0
 END
 
 # make archieve
