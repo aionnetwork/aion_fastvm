9829cfa214afa75c532ec72aafe5f445d96e3f07
aion-kelvin <kelvin@aion.network>
2018-09-18 10:57:06 -0400

    Put back module-info.java (don't know why it was removed)

---
+++ modFastVM/src/module-info.java
@@ -0,0 +1,12 @@
+module aion.fastvm {
+    requires aion.base;
+    requires aion.mcf;
+    requires slf4j.api;
+    requires aion.crypto;
+    requires aion.zero;
+    requires aion.vm;
+    requires aion.precompiled;
+
+    exports org.aion.solidity;
+    exports org.aion.fastvm;
+}
