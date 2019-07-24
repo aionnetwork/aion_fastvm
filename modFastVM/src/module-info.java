2a12161ef398de51014c8d15aec3b300742eeb3e
aionick <nick@nuco.io>
2019-07-24 09:48:09 -0400

    Remove some fvm dependencies
    - We are now able to remove every dependency except for the one on
      modBase.

--- modFastVM/src/module-info.java
+++ modFastVM/src/module-info.java
@@ -1,12 +1,7 @@
 module aion.fastvm {
-    requires aion.mcf;
     requires slf4j.api;
-    requires aion.crypto;
-    requires aion.zero;
-    requires aion.precompiled;
     requires commons.lang3;
     requires commons.collections4;
-    requires aion.util;
     requires org.json;
     requires aion.types;
     requires aion.base;
